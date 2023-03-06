4520be1235afcabdfe10cd0e68218c3bf87b33ce
benjaminhate <benjamin.hate.09@gmail.com>
2023-03-06 23:21:30 +0100

    feat: Compare charts multi users

--- client/public/js/compare/charts.js
+++ client/public/js/compare/charts.js
@@ -1,4 +1,7 @@
-const compareChartsUsernameInput = document.getElementById("usernameCharts");
+const compareChartsUsername1Input = document.getElementById("usernameCharts1");
+const compareChartsUsername2Input = document.getElementById("usernameCharts2");
+const compareChartsUsername3Input = document.getElementById("usernameCharts3");
+const compareChartsAxisSelect = document.getElementById("compareChartsAxisSelect");
 
 const compareChartsLoader = document.getElementById("compareChartsLoader");
 const compareChartsContent = document.getElementById("compareChartsContent");
@@ -8,6 +11,10 @@ const config = {
     options: {
         parsing: {
             xAxisKey: 'id'
+        },
+        interaction: {
+            intersect: false,
+            mode: 'index',
         }
     }
 };
@@ -24,76 +31,135 @@ async function compareChartsLoad(){
     
     hideElement(compareChartsContent);
 
-    let username = compareChartsUsernameInput.value;
+    let username1 = compareChartsUsername1Input.value;
+    let username2 = compareChartsUsername2Input.value;
+    let username3 = compareChartsUsername3Input.value;
 
     showElement(compareChartsLoader);
 
-    let json = await getUserData(username);
+    let jsons = [];
+
+    if(username1){
+        let json = await getUserData(username1);
+        if(json.success){
+            jsons.push(json.result)
+        }else{
+            alert(`Error : ${json.result.msg}`);
+        }
+    }
+    if(username2){
+        let json = await getUserData(username2);
+        if(json.success){
+            jsons.push(json.result)
+        }else{
+            alert(`Error : ${json.result.msg}`);
+        }
+    }
+    if(username3){
+        let json = await getUserData(username3);
+        if(json.success){
+            jsons.push(json.result)
+        }else{
+            alert(`Error : ${json.result.msg}`);
+        }
+    }
+    
+    compareChartsFillCharts(jsons);
 
     hideElement(compareChartsLoader);
-
-    if(json.success){
-        compareChartsFillCharts(json.result);
-        showElement(compareChartsContent);
-    }else{
-        alert(`Error : ${json.result.msg}`);
-    }
+    showElement(compareChartsContent);
+    
 }
 
 function compareChartsCheckInputs(){
-    if(compareChartsUsernameInput === undefined)
+    if(compareChartsUsername1Input === undefined
+        && compareChartsUsername2Input === undefined
+        && compareChartsUsername3Input === undefined)
         return false;
     
-    if(!compareChartsUsernameInput.value)
+    if(!compareChartsUsername1Input.value
+        && !compareChartsUsername2Input.value
+        && !compareChartsUsername3Input.value)
         return false;
 
     return true;
 }
 
-function compareChartsFillCharts(userData){
-    const deezerData = [];
-    for(let data of userData.deezerData){
-        for(let monthData of data.monthsData){
+function compareChartsFillCharts(userDatas){
+    const monthsYears = userDatas.flatMap(u => u.deezerData.flatMap(d => d.monthsData.flatMap(m => {
+        return {
+            year: d.year,
+            month: common.months.getMonthNumberFromName(m.month)
+        };
+    })))
+        .sort((a, b) => {
+            if(a.year < b.year) return -1;
+            if(a.year > b.year) return 1;
+            if(a.year == b.year){
+                if(a.month < b.month) return -1;
+                if(a.month > b.month) return 1;
+            }
+            return 0;
+        })
+        .filter((v, i, a) => a.indexOf(a.find(item => item.year == v.year && item.month == v.month)) === i);
+
+    const deezerUserData = [];
+    for(let user of userDatas){
+        let deezerData = [];
+        for(let monthYear of monthsYears){
+            let userData = findUserData(user.deezerData, monthYear.year, monthYear.month);
             deezerData.push({
-                id: `${data.year}-${monthData.month}`,
-                songs: monthData.songs,
-                minutes: monthData.minutes,
-                songsPerDay: monthData.songsPerDay
+                id: `${common.months.getMonthNameFromNumber(monthYear.month)} ${monthYear.year}`,
+                songs: userData?.songs,
+                minutes: userData?.minutes,
+                songsPerDay: userData?.songsPerDay
             })
         }
+        deezerUserData.push({
+            user: user.name,
+            data: deezerData
+        })
     }
 
-    compareChart.data.datasets = [
-        {
-            label: 'Chansons écoutées',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: deezerData,
+    compareChart.data.datasets = deezerUserData.map((d, i) => {
+        return {
+            label: d.user,
+            backgroundColor: getChartColor(i),
+            borderColor: getChartColor(i),
+            data: d.data,
             parsing: {
-                yAxisKey: 'songs'
-            }
-        },
-        {
-            label: 'Minutes écoutées',
-            hidden: true,
-            backgroundColor: 'rgb(132, 255, 99)',
-            borderColor: 'rgb(132, 255, 99)',
-            data: deezerData,
-            parsing: {
-                yAxisKey: 'minutes'
+                yAxisKey: compareChartsAxisSelect?.value ?? 'songs'
             },
-        },
-        {
-            label: 'Chansons écoutées par jour',
-            hidden: true,
-            backgroundColor: 'rgb(99, 132, 255)',
-            borderColor: 'rgb(99, 132, 255)',
-            data: deezerData,
-            parsing: {
-                yAxisKey: 'songsPerDay'
-            },
-        }
-    ];
+            spanGaps: true,
+        };
+    });
     
+    compareChart.update();
+}
+
+function getChartColor(index){
+    switch (index) {
+        case 0:
+            return 'rgb(255, 99, 132)';
+        case 1:
+            return 'rgb(132, 255, 99)';
+        case 2:
+            return 'rgb(99, 132, 255)';
+        default:
+            return 'rgb(0, 0, 0)'
+    }
+}
+
+function findUserData(userData, year, month){
+    let userYearData = userData.find(d => d.year == year);
+    if(!userYearData) return null;
+    let userMonthData = userYearData.monthsData.find(d => common.months.getMonthNumberFromName(d.month) == month)
+    return !userMonthData ? null : userMonthData;
+}
+
+function changeChartAxis(){
+    for(let dataset of compareChart.data.datasets){
+        dataset.parsing.yAxisKey = compareChartsAxisSelect?.value ?? 'songs'
+    }
     compareChart.update();
 }
